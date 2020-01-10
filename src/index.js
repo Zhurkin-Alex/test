66ead2b7c3f6abd90fb4d32615356357fda294a5
Aleksey <lexus1812141@gmail.com>
2020-01-11 01:37:18 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import sum from './module/sum';
+import guard from './module/guard';
+
+console.log(sum(1,30));
+console.log(guard('Alexey'));
+
---
+++ src/module/guard.js
@@ -0,0 +1,7 @@
+export default function(name) {
+    if (name == 'Alexey') {
+        return 'Hi';
+    } else {
+        return 'get out';
+    }
+};
